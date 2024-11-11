fbbfd67e6b087d416aaa40aa772ed1c77939178d
furkangurbuz <furkangurbuz5@gmail.com>
2024-11-11 16:13:35 +0100

    routing and css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { WorkoutComponent } from './workout/workout.component';
 import { PersonComponent } from './person/person.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 export const routes: Routes = [
         {path: 'workout', component: WorkoutComponent},
         {path: 'person', component: PersonComponent},
-        {path: '', redirectTo: '/workout', pathMatch: 'full'}
-
-
+        {path: '', redirectTo: '/workout', pathMatch: 'full'},
+        {path: '**', component: NotfoundComponent}
 ];
