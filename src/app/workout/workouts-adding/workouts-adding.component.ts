e7cb877ba92d9044429458821a94016e7fb7075f
furkangurbuz <furkangurbuz5@gmail.com>
2024-11-12 11:04:52 +0100

    changed colours

--- src/app/workout/workouts-adding/workouts-adding.component.ts
+++ src/app/workout/workouts-adding/workouts-adding.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'pow-workouts-adding',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './workouts-adding.component.html',
   styleUrl: './workouts-adding.component.css'
 })
-export class WorkoutsAddingComponent {
+export class WorkoutsAddingComponent implements OnInit{
+
+        id: number = 0; //default
+        id$ : Observable<number>;
+
+        constructor(private router: ActivatedRoute){
+                this.id$ = this.router.params.pipe(
+                        map(params => params['id'])
+                );
+        }
+
+        ngOnInit(): void{
+                //this.id = this.router.snapshot.params['id']; //this will never receive a new value
+                this.id$ = this.router.params.pipe(
+                        map(params => params['id'])
+                );
+
+                //this.router.params.subscribe((params) => { this.id = params['id']; });
+        }
 
 }
