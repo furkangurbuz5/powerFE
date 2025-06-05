32b6911f4abea9316955bf6af7e4846bf42efabd
furkang-risa <Furkan@risa-it.nl>
2025-06-05 15:09:53 +0200

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,69 @@
-import { Component, ElementRef, Inject, OnInit, Optional, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  Optional,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WorkoutComponent } from "./workout/workout.component";
+import { WorkoutComponent } from './workout/workout.component';
 import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
-import { ContainerComponent } from "./container/container.component";
-import { PersonComponent } from "./person/person.component";
+import { ContainerComponent } from './container/container.component';
+import { PersonComponent } from './person/person.component';
 import { LoggerService } from './services/logger.service';
 import { localStorageToken } from './localstorage.token';
 import { RouterModule } from '@angular/router';
-import { AppNavComponent } from "./app-nav/app-nav.component";
+import { AppNavComponent } from './app-nav/app-nav.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-        selector: 'pow-root',
-        standalone: true,
-        imports: [RouterOutlet, WorkoutComponent, NgSwitch, NgSwitchCase, NgSwitchDefault, ContainerComponent, PersonComponent, RouterModule, AppNavComponent, FormsModule],
-        templateUrl: './app.component.html',
-        //   template: `<h1>Hello World from inline template!</h1>
-        //   <p> Angular P tag multi line test</p>
-        //   `,
-        styleUrl: './app.component.css',
-        styles: [`h1 {color: purple}`]
+  selector: 'pow-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    WorkoutComponent,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    ContainerComponent,
+    PersonComponent,
+    RouterModule,
+    AppNavComponent,
+    FormsModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  styles: [
+    `
+      h1 {
+        color: purple;
+      }
+    `,
+  ],
 })
 export class AppComponent implements OnInit {
-        
-        title = 'powerFE';
-        role = 'admin';
+  title = 'powerFE';
+  role = 'admin';
 
-        @ViewChild('apptitle', {static: true}) apptitle!: ElementRef;
+  @ViewChild('apptitle', { static: true }) apptitle!: ElementRef;
 
-        constructor(@Optional() private loggerService: LoggerService,
-                @Inject(localStorageToken) private localStorage: any){
-                
-                
-        }
+  constructor(
+    @Optional() private loggerService: LoggerService,
+    @Inject(localStorageToken) private localStorage: any
+  ) {}
 
-        ngOnInit(): void {
-                this.loggerService?.log('AppComponent.ngOnInit()');
-                this.apptitle.nativeElement.innerText = "Powerlifting, Esq.";
-                this.localStorage.setItem('name', 'Powerlifting, Esq.');
-        }
+  ngOnInit(): void {
+    this.loggerService?.log('AppComponent.ngOnInit()');
+    this.apptitle.nativeElement.innerText = 'Powerlifting, Esq.';
+    this.localStorage.setItem('name', 'Powerlifting, Esq.');
+  }
 
+  // @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
 
-        // @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
-
-        // ngAfterViewInit(): void {
-        //         const componentRef = this.vcr.createComponent(WorkoutComponent);
-        //         componentRef.instance.currentGym = 'Sport City Capelle a/d IJssel';
-        // }
-
-        
-
+  // ngAfterViewInit(): void {
+  //         const componentRef = this.vcr.createComponent(WorkoutComponent);
+  //         componentRef.instance.currentGym = 'Sport City Capelle a/d IJssel';
+  // }
 }
--- src/app/workout/workout.component.ts
+++ src/app/workout/workout.component.ts
@@ -1,110 +1,133 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Input, OnChanges, OnInit, QueryList, SimpleChanges, SkipSelf, ViewChild, ViewChildren, } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnInit,
+  QueryList,
+  SimpleChanges,
+  SkipSelf,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { Workout, WorkoutList } from './workouts';
 import { JsonPipe, NgClass, NgFor, NgIf, PercentPipe } from '@angular/common';
 import { WorkoutsListComponent } from './workouts-list/workouts-list.component';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../header/header.component';
 import { WorkoutsService } from '../services/workouts.service';
 import { Observable, Subscription } from 'rxjs';
 import { RouterModule } from '@angular/router';
 
-
 @Component({
-        selector: 'pow-workout',
-        standalone: true,
-        imports: [NgIf, NgFor, NgClass, PercentPipe, WorkoutsListComponent, JsonPipe, HeaderComponent, RouterModule],
-        templateUrl: './workout.component.html',
-        styleUrls: ['./workout.component.css'],
-        changeDetection: ChangeDetectionStrategy.Default
+  selector: 'pow-workout',
+  standalone: true,
+  imports: [
+    NgIf,
+    NgFor,
+    NgClass,
+    PercentPipe,
+    WorkoutsListComponent,
+    JsonPipe,
+    HeaderComponent,
+    RouterModule,
+  ],
+  templateUrl: './workout.component.html',
+  styleUrls: ['./workout.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class WorkoutComponent implements OnInit, AfterViewInit {
+  appName = 'Power';
+  currentGym = 'Sport City Houtrust';
+  hideGym = false;
 
-        appName = 'Power';
-        currentGym = 'Sport City Houtrust';
-        hideGym = false;
+  selectedWorkout!: WorkoutList;
+  title = 'Workout List';
+  workoutList: WorkoutList[] = [];
 
-        selectedWorkout!: WorkoutList;
-        title = 'Workout List';
-        workoutList: WorkoutList[] = [];
+  stream = new Observable<string>((observer) => {
+    //whenever new value is pushed, observer will observe the new value.
+    observer.next('user1');
+    observer.next('user2');
+    observer.next('user3');
+    observer.complete();
+    // observer.error('error');
+  });
 
-        stream = new Observable<string>(observer => { //whenever new value is pushed, observer will observe the new value. 
-                observer.next('user1');
-                observer.next('user2');
-                observer.next('user3');
-                observer.complete();
-                // observer.error('error');
-        });
+  subscription!: Subscription;
 
-        subscription !: Subscription;
+  @ViewChild(HeaderComponent, { static: false })
+  headerComponent!: HeaderComponent;
+  @ViewChildren(HeaderComponent)
+  headerChildrenComponent!: QueryList<HeaderComponent>;
 
-        @ViewChild(HeaderComponent, { static: false }) headerComponent!: HeaderComponent;
-        @ViewChildren(HeaderComponent) headerChildrenComponent!: QueryList<HeaderComponent>;
+  //dependency injection: workout service
+  constructor(
+    private workoutService: WorkoutsService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-        //dependency injection: workout service
-        constructor(private workoutService: WorkoutsService, private cdr: ChangeDetectorRef) { }
+  ngAfterViewInit(): void {
+    this.headerComponent.title = 'Powerlifting, Esq.';
+  }
 
-        ngAfterViewInit(): void {
-                this.headerComponent.title = "Powerlifting, Esq.";
-        }
+  ngOnInit(): void {
+    this.stream.subscribe({
+      next: (value) => console.log(value),
+      complete: () => console.log('complete'),
+      error: (err) => console.log(err),
+    });
+    //this.stream.subscribe((data) => console.log(data));
 
+    //using the api to get workouts and add to list
+    this.workoutService.getWorkouts().subscribe((workouts) => {
+      console.log('Fetched workouts: ', workouts);
+      this.workoutList = workouts;
+    });
+  }
 
-        ngOnInit(): void {
-                this.stream.subscribe({
-                        next: (value) => console.log(value),
-                        complete: () => console.log('complete'),
-                        error: (err) => console.log(err),
-                });
-                //this.stream.subscribe((data) => console.log(data));
+  toggle() {
+    this.hideGym = !this.hideGym;
+    this.title = 'Workouts List';
+  }
 
-                //using the api to get workouts and add to list
-                this.workoutService.getWorkouts().subscribe((workouts) => {
-                        console.log("Fetched workouts: ", workouts);
-                        this.workoutList = workouts;
-                });
-        }
+  selectWorkout(workout: WorkoutList) {
+    this.selectedWorkout = workout;
+  }
 
-        toggle() {
-                this.hideGym = !this.hideGym;
-                this.title = "Workouts List";
-        }
+  editWorkout() {
+    const workout: WorkoutList = {
+      id: 1,
+      title: 'Overhead press',
+      startedOn: new Date('November 29, 2024 18:30:00'),
+      completedOn: new Date('November 29, 2024, 19:30:00'),
+      workoutsDone: 1,
+      location: 'Sport City Houtrust',
+    };
 
-        selectWorkout(workout: WorkoutList) {
-                this.selectedWorkout = workout;
-        }
+    this.workoutService.editWorkout(workout).subscribe((data) => {
+      this.workoutList = data;
+    });
+  }
 
-        editWorkout(){
-                const workout: WorkoutList = {
-                        id: 1,
-                        title: 'Overhead press',
-                        startedOn: new Date("November 29, 2024 18:30:00"),
-                        completedOn: new Date("November 29, 2024, 19:30:00"),
-                        workoutsDone: 1,
-                        location: 'Sport City Houtrust'
-                };
+  addWorkout() {
+    const workout: WorkoutList = {
+      id: 6,
+      title: 'Barbell squat',
+      startedOn: new Date('November 29, 2024 18:30:00'),
+      completedOn: new Date('November 29, 2024, 19:30:00'),
+      workoutsDone: 1,
+      location: 'Sport City Houtrust',
+    };
+    this.workoutService.addWorkout(workout).subscribe((data) => {
+      this.workoutList = data;
+      this.cdr.detectChanges();
+    });
+  }
 
-                this.workoutService.editWorkout(workout).subscribe((data) => {
-                        this.workoutList = data;
-                });
-        }
-
-        addWorkout() {
-                const workout: WorkoutList = {
-                        id: 6,
-                        title: 'Barbell squat',
-                        startedOn: new Date("November 29, 2024 18:30:00"),
-                        completedOn: new Date("November 29, 2024, 19:30:00"),
-                        workoutsDone: 1,
-                        location: 'Sport City Houtrust'
-                };
-                this.workoutService.addWorkout(workout).subscribe((data) => {
-                        this.workoutList = data;
-                        this.cdr.detectChanges();
-                });
-        }
-
-        deleteWorkout(){
-                this.workoutService.deleteWorkout(6).subscribe((data) => {
-                });
-        }
-
-        
+  deleteWorkout() {
+    this.workoutService.deleteWorkout(6).subscribe((data) => {});
+  }
 }
